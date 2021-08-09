073b9127caf5f2bf37cc293da0c7f733b3409d23
Davon Belvin <davonbelvin@gmail.com>
2021-08-09 13:32:58 -0700

    initial commit

---
+++ src/test/java/com/smoothhunger/donate/NonprofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothhunger.donate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonprofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
