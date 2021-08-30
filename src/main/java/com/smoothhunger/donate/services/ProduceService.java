0e418277e1b904c1a1d68b7a560e990dec7f11df
Davon Belvin <davonbelvin@gmail.com>
2021-08-30 10:26:34 -0700

    Seedeed data from CSV successfully.

--- src/main/java/com/smoothhunger/donate/services/ProduceService.java
+++ src/main/java/com/smoothhunger/donate/services/ProduceService.java
@@ -1,5 +1,7 @@
 package com.smoothhunger.donate.services;
 
+
+import com.smoothhunger.donate.models.Produce;
 import com.smoothhunger.donate.repositories.ProduceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,10 @@ public class ProduceService {
 
     @Autowired
     public ProduceService(ProduceRepository produceRepository){this.produceRepository =produceRepository;}
+
+    public Produce save(Produce produceToPersist) {
+        return produceRepository.save(produceToPersist);
+    }
 
     public long count() {
         return produceRepository.count();
